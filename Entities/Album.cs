798e5ad72fa40b830a5dc556c916326fe94150f2
LoganStyles <emmaist23@gmail.com>
2022-07-08 17:18:10 +0100

    feat(oneToOne): configured album & employee entities to reflect single navigation property using the collection property

--- Entities/Album.cs
+++ Entities/Album.cs
@@ -5,7 +5,6 @@ namespace Relationships
         public long Id { get; set; }
         public string Title { get; set; }
         public double Price { get; set; }
-        public Employee Employee { get; set; }
     }
 }
 
