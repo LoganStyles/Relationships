36f99da36ab05fa1e0d547149c51b5abcf4cbfb3
LoganStyles <emmaist23@gmail.com>
2022-07-08 16:45:08 +0100

    feat(context): added ArtistsContext.cs

---
+++ Data/ArtistsContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Relationships.Data
+{
+    public partial class ArtistsContext : DbContext
+    {
+        public ArtistsContext() { }
+
+        public ArtistsContext(DbContextOptions<ArtistsContext> options) : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Album> Albums { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("data source=output/Artists.db");
+            }
+        }
+    }
+}
