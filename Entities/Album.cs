3243d6e61c08e96a10edf6d39ab3fbebc920d427
LoganStyles <emmaist23@gmail.com>
2022-07-08 22:23:04 +0100

    feat(fully defined relationship): added reference navigation for Employee as well as EmployeeId as foreign key

--- Entities/Album.cs
+++ Entities/Album.cs
@@ -5,6 +5,8 @@ namespace Relationships
         public long Id { get; set; }
         public string Title { get; set; }
         public double Price { get; set; }
+        public long EmployeeId { get; set; }
+        public Employee Employee { get; set; }
     }
 }
 
