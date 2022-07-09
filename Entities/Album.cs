32b3d927a52896cd32b68a6621264bad8e8ba029
LoganStyles <emmaist23@gmail.com>
2022-07-09 14:20:08 +0100

    feat(fully defined): added line break to separate highlighted properties

--- Entities/Album.cs
+++ Entities/Album.cs
@@ -5,6 +5,7 @@ namespace Relationships
         public long Id { get; set; }
         public string Title { get; set; }
         public double Price { get; set; }
+        
         public long EmployeeId { get; set; }
         public Employee Employee { get; set; }
     }
